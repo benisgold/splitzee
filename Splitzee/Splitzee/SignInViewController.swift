3a2c94bdd2ac4f777b0dea00776e21b5384d991c
Vidya Ravikumar <vravikumar@berkeley.edu>
2016-11-17 15:04:29 -0800

    added UI to signing view controller

--- Splitzee/Splitzee/SignInViewController.swift
+++ Splitzee/Splitzee/SignInViewController.swift
@@ -22,7 +22,7 @@ class SignInViewController: UIViewController {
     var signInGoogle: UIButton!
     var signInFacebook: UIButton!
     var background: UIImageView!
-    
+    let constants = Constants()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,30 +46,53 @@ class SignInViewController: UIViewController {
         
         // splitzeeLogo
         splitzeeLogo = UIImageView(image: #imageLiteral(resourceName: "splitzeeStraight"))
-        splitzeeLogo.frame = CGRect(x: 0, y: 131, width: view.frame.width*0.389, height: view.frame.height*0.205)
+        splitzeeLogo.frame = CGRect(x: 0, y: view.frame.height * -0.01, width: view.frame.width, height: view.frame.height * 0.280)
+        splitzeeLogo.contentMode = .scaleAspectFit
         self.view.addSubview(splitzeeLogo)
         
         // pleaseSignIn
         
         // plainDividingLine
         plainDividingLine = UIImageView(image: #imageLiteral(resourceName: "line"))
-        plainDividingLine.frame = CGRect(x: 40, y: 208, width: 338, height: 3)
+        plainDividingLine.frame = CGRect(x: view.frame.width * 0.098, y: view.frame.height * 0.283, width: view.frame.width * 0.804, height: view.frame.height * 0.01)
+        plainDividingLine.contentMode = .scaleAspectFill
         self.view.addSubview(plainDividingLine)
         
         // inputEmailOrUsername
         inputEmailOrUsername = UITextField()
-        let inputEmailOrUsernamePlaceholder = NSAttributedString(string: "Email or username", attributes: [NSForegroundColorAttributeName: UIColor.lightGray])
+        let inputEmailOrUsernamePlaceholder = NSAttributedString(string: String(describing: "Email or username"), attributes: [NSForegroundColorAttributeName: UIColor.lightGray])
         inputEmailOrUsername.attributedPlaceholder = inputEmailOrUsernamePlaceholder
-        inputEmailOrUsername.frame = CGRect(x: view.frame.width*0.077, y: view.frame.height*0.329, width: view.frame.width*0.845, height: view.frame.height*0.057)
-        // in progress!!!!! --------------
+        inputEmailOrUsername.frame = CGRect(x: view.frame.width * 0.077, y: view.frame.height * 0.329, width: view.frame.width * 0.841, height: view.frame.height * 0.057)
+        inputEmailOrUsername.backgroundColor = UIColor.white
+        view.addSubview(inputEmailOrUsername)
+        // in progress!!!!! -------------- need to add picture to text
         
         // inputPassword
+        inputPassword = UITextField(frame: CGRect(x: view.frame.width * 0.077, y: view.frame.height * 0.410, width: view.frame.width * 0.841, height: view.frame.height * 0.057))
+        let inputPasswordPlaceholder = NSAttributedString(string: String(describing: "Password"), attributes: [NSForegroundColorAttributeName: UIColor.lightGray])
+        inputPassword.attributedPlaceholder = inputPasswordPlaceholder
+        inputPassword.backgroundColor = UIColor.white
+        view.addSubview(inputPassword)
+        // in progress!!!!! -------------- need to add picture to text
         
         // signinButton
+        signInButton = UIButton(frame: CGRect(x: view.frame.width * 0.077, y: view.frame.height * 0.503, width: view.frame.width * 0.256, height: view.frame.height * 0.068))
+        signInButton.setTitle("SIGN IN", for: .normal)
+        signInButton.backgroundColor = constants.darkGray
+        view.addSubview(signInButton)
         
         // createAccountButton
+        createAccountButton = UIButton(frame: CGRect(x: view.frame.width * 0.350, y: view.frame.height * 0.503, width: view.frame.width * 0.568, height: view.frame.height * 0.068))
+        createAccountButton.setTitle("CREATE ACCOUNT", for: .normal)
+        createAccountButton.backgroundColor = constants.red
+        view.addSubview(createAccountButton)
         
         // forgotPassword
+        forgotPassword = UILabel(frame: CGRect(x: view.frame.width * 0.325, y: view.frame.height * 0.586, width: view.frame.width * 0.330, height: view.frame.height * 0.023))
+        forgotPassword.text = "Forgot password?"
+        forgotPassword.textAlignment = .center
+        forgotPassword.textColor = constants.fontDarkGray
+        view.addSubview(forgotPassword)
         
         // orDividingLine
         
