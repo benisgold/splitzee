5211088212a72196b3e703c0ed498ff8afdba19b
Ben Goldberg <benisgold@gmail.com>
2016-11-20 02:06:25 -0800

    working on SignInVC

--- Splitzee/Splitzee/SignInViewController.swift
+++ Splitzee/Splitzee/SignInViewController.swift
@@ -47,12 +47,12 @@ class SignInViewController: UIViewController {
         
         // splitzeeLogo
         splitzeeLogo = UIImageView(image: #imageLiteral(resourceName: "splitzeeStraight"))
-        splitzeeLogo.frame = CGRect(x: view.frame.width * 0.052, y: view.frame.height * -0.01, width: view.frame.width, height: view.frame.height * 0.250)
+        splitzeeLogo.frame = CGRect(x: view.frame.width * 0.052, y: view.frame.height * -0.045, width: view.frame.width, height: view.frame.height * 0.250)
         splitzeeLogo.contentMode = .scaleAspectFit
         self.view.addSubview(splitzeeLogo)
         
         // pleaseSignIn
-        pleaseSignIn = UILabel(frame: CGRect(x: 0, y: view.frame.height * 0.245, width: view.frame.width, height: view.frame.height * 0.043))
+        pleaseSignIn = UILabel(frame: CGRect(x: 0, y: view.frame.height * 0.230, width: view.frame.width, height: view.frame.height * 0.043))
         pleaseSignIn.textColor = UIColor.white
         pleaseSignIn.font = UIFont(name: "SFUIText-Light", size: 27)
         pleaseSignIn.text = "Sign in"
@@ -67,36 +67,47 @@ class SignInViewController: UIViewController {
         
         // inputEmail
         inputEmail = UITextField()
-        let inputEmailPlaceholder = NSAttributedString(string: String(describing: "     Email"), attributes: [NSForegroundColorAttributeName: UIColor.lightGray, NSFontAttributeName: UIFont(name: "SFUIText-Light", size: 14)!])
+        let inputEmailPlaceholder = NSAttributedString(string: String(describing: "Email"), attributes: [NSForegroundColorAttributeName: UIColor.lightGray])
+        inputEmail.font = UIFont(name: "SFUIText-Light", size: 14)
         inputEmail.attributedPlaceholder = inputEmailPlaceholder
         inputEmail.frame = CGRect(x: view.frame.width * 0.077, y: view.frame.height * 0.329, width: view.frame.width * 0.841, height: view.frame.height * 0.057)
         inputEmail.backgroundColor = UIColor.white
+        inputEmail.layer.cornerRadius = 3
+        createInset(textField: inputEmail)
         view.addSubview(inputEmail)
-        // in progress!!!!! -------------- need to add picture to text
+        
+//        // mailSymbol
+//        let mailImage = UIImageView(image: #imageLiteral(resourceName: "mailSymbol"))
+//        mailImage.frame = CGRect(x: 0.830 * view.frame.width, y: 0.344 * view.frame.height, width: 0.140 * view.frame.width, height: 0.038 * view.frame.height)
+//        view.addSubview(mailImage)
         
         // inputPassword
         inputPassword = UITextField(frame: CGRect(x: view.frame.width * 0.077, y: view.frame.height * 0.410, width: view.frame.width * 0.841, height: view.frame.height * 0.057))
-        let inputPasswordPlaceholder = NSAttributedString(string: String(describing: "     Password"), attributes: [NSForegroundColorAttributeName: UIColor.lightGray, NSFontAttributeName: UIFont(name: "SFUIText-Light", size: 14)!])
+        inputPassword.font = UIFont(name: "SFUIText-Light", size: 14)
+        let inputPasswordPlaceholder = NSAttributedString(string: String(describing: "Password"), attributes: [NSForegroundColorAttributeName: UIColor.lightGray])
         inputPassword.attributedPlaceholder = inputPasswordPlaceholder
         inputPassword.backgroundColor = UIColor.white
+        inputPassword.textColor = UIColor.black
+        inputPassword.layer.cornerRadius = 3
+        createInset(textField: inputPassword)
         view.addSubview(inputPassword)
         // in progress!!!!! -------------- need to add picture to text
         
         // signinButton
         signInButton = UIButton(frame: CGRect(x: view.frame.width * 0.077, y: view.frame.height * 0.503, width: view.frame.width * 0.256, height: view.frame.height * 0.068))
         signInButton.setTitle("SIGN IN", for: .normal)
-        signInButton.titleLabel?.font = UIFont(name: "SFUIText-Light", size: 14)
+        signInButton.titleLabel?.font = UIFont(name: "SFUIText-Light", size: 18)
         signInButton.backgroundColor = constants.darkGray
-        
+        signInButton.layer.cornerRadius = 3
         signInButton.addTarget(self, action: #selector(touchSignInButton), for: .touchUpInside)
         view.addSubview(signInButton)
         
         // createAccountButton
         createAccountButton = UIButton(frame: CGRect(x: view.frame.width * 0.350, y: view.frame.height * 0.503, width: view.frame.width * 0.568, height: view.frame.height * 0.068))
         createAccountButton.setTitle("CREATE ACCOUNT", for: .normal)
-        createAccountButton.titleLabel?.font = UIFont(name: "SFUIText-Light", size: 14)
+        createAccountButton.titleLabel?.font = UIFont(name: "SFUIText-Light", size: 18)
         createAccountButton.backgroundColor = constants.red
-        
+        createAccountButton.layer.cornerRadius = 3
         createAccountButton.addTarget(self, action: #selector(touchCreateAccountButton), for: .touchUpInside)
         view.addSubview(createAccountButton)
         
@@ -110,7 +121,8 @@ class SignInViewController: UIViewController {
         
         // orDividingLine
         orDividingLine = UIImageView(image: #imageLiteral(resourceName: "orLine"))
-        orDividingLine.frame = CGRect(x: view.frame.width * 0.098, y: view.frame.height * 0.650, width: view.frame.width * 0.804, height: view.frame.height * 0.01)
+        orDividingLine.frame = CGRect(x: view.frame.width * 0.098, y: view.frame.height * 0.641
+            , width: view.frame.width * 0.804, height: view.frame.height * 0.01)
         orDividingLine.contentMode = .scaleAspectFill
         self.view.addSubview(orDividingLine)
         
@@ -120,6 +132,7 @@ class SignInViewController: UIViewController {
         signInGoogle.titleLabel?.font = UIFont(name: "SFUIText-Regular", size: 18)
         signInGoogle.backgroundColor = UIColor.clear
         signInGoogle.layer.borderWidth = 1
+        signInGoogle.layer.cornerRadius = 3
         signInGoogle.layer.borderColor = UIColor.white.cgColor
         view.addSubview(signInGoogle)
         
@@ -129,9 +142,19 @@ class SignInViewController: UIViewController {
         signInFacebook.titleLabel?.font = UIFont(name: "SFUIText-Regular", size: 18)
         signInFacebook.backgroundColor = UIColor.clear
         signInFacebook.layer.borderWidth = 1
+        signInFacebook.layer.cornerRadius = 3
         signInFacebook.layer.borderColor = UIColor.white.cgColor
         view.addSubview(signInFacebook)
 
+    }
+    
+    func createInset(textField: UITextField) {
+        let Inset = UITextView()
+        Inset.frame = CGRect(x: 0, y: 0, width: 0.048 * view.frame.width, height: textField.frame.height)
+        Inset.layer.cornerRadius = 3
+        textField.leftView = Inset
+        textField.leftViewMode = .always
+        view.addSubview(Inset)
     }
     
 // ---------------FUNCTIONS---------------------------------------------------------------
