dd8b3ce1d3b5f444d486086f5751367eef0d2d25
Vidya Ravikumar <vravikumar@berkeley.edu>
2016-11-28 20:02:42 -0800

    formatting adminPage cells

--- Splitzee/Splitzee/AdminPendingTableViewCell.swift
+++ Splitzee/Splitzee/AdminPendingTableViewCell.swift
@@ -39,43 +39,43 @@ class AdminPendingTableViewCell: UITableViewCell {
     
     func makeMemberNameLabel() {
         memberNameLabel = UILabel()
-        memberNameLabel.frame = CGRect(x: 0.251 * contentView.frame.width, y: 0.082 * contentView.frame.height, width: 0.176 * contentView.frame.width, height: 0.173 * contentView.frame.height)
+        memberNameLabel.frame = CGRect(x: 0.251 * contentView.frame.width, y: 0.082 * contentView.frame.height, width: 0.749 * contentView.frame.width, height: 0.331 * contentView.frame.height)
+        memberNameLabel.text = "Jane Doe"
         memberNameLabel.textColor = constants.fontMediumDarkBlue
-        memberNameLabel.font = UIFont(name: "SFUIText-Semibold", size: 16)
+        memberNameLabel.font = UIFont(name: "SFUIText-Semibold", size: 10)
         contentView.addSubview(memberNameLabel)
     }
     
     func makeDescriptionLabel() {
         descriptionLabel = UILabel()
-        descriptionLabel.frame = CGRect(x: 0.251 * contentView.frame.width, y: 0.273 * contentView.frame.height, width: 0.696 * contentView.frame.width, height: 0.291 * contentView.frame.height)
+        descriptionLabel.frame = CGRect(x: 0.251 * contentView.frame.width, y: 0.336 * contentView.frame.height, width: 0.696 * contentView.frame.width, height: 0.331 * contentView.frame.height)
+        descriptionLabel.textColor = UIColor.blue
+        descriptionLabel.text = "Hello. My name is Jane Doe. I love computer science."
         descriptionLabel.textColor = constants.fontMediumDarkBlue
-        memberNameLabel.font = UIFont(name: "SFUIText-LightItalic", size: 14)
+        descriptionLabel.font = UIFont(name: "SFUIText-LightItalic", size: 10)
         contentView.addSubview(descriptionLabel)
     }
     
     func makeRejectButton() {
         rejectButton = UIButton()
-        rejectButton.frame = CGRect(x: 0.248 * contentView.frame.width, y: 0.633 * contentView.frame.height, width: 0.340 * contentView.frame.width, height: 0.291 * contentView.frame.height)
+        rejectButton.frame = CGRect(x: 0.248 * contentView.frame.width, y: 0.672 * contentView.frame.height, width: 0.340 * contentView.frame.width, height: 0.328 * contentView.frame.height)
+        rejectButton.setTitle("Reject", for: .normal)
         rejectButton.layer.borderColor = constants.lightBlue.cgColor
         rejectButton.titleLabel?.textColor = constants.fontMediumDarkBlue
-        rejectButton.titleLabel?.font = UIFont(name: "SFUIText-Medium", size: 14)
+        rejectButton.titleLabel?.font = UIFont(name: "SFUIText-Medium", size: 10)
         rejectButton.layer.cornerRadius = 3
-        
         contentView.addSubview(rejectButton)
     }
     
     func makeApproveButton() {
         approveButton = UIButton()
-        approveButton.frame = CGRect(x: 0.603 * contentView.frame.width, y: 0.633 * contentView.frame.height, width: 0.340 * contentView.frame.width, height: 0.291 * contentView.frame.height)
+        approveButton.frame = CGRect(x: 0.603 * contentView.frame.width, y: 0.672 * contentView.frame.height, width: 0.340 * contentView.frame.width, height: 0.328 * contentView.frame.height)
         approveButton.titleLabel?.textColor = UIColor.white
-        approveButton.titleLabel?.font = UIFont(name: "SFUIText-Medium", size: 14)
+        approveButton.titleLabel?.font = UIFont(name: "SFUIText-Medium", size: 10)
         approveButton.layer.cornerRadius = 3
+        approveButton.setTitle("APPROVE!!", for: .normal)
         contentView.addSubview(approveButton)
-        
     }
-   
-    
-     
     
 }
  
