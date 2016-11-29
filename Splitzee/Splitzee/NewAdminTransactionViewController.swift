15204a3f243ad6112834a523d58db7b897645508
Shaan Appel <shaan.appel@yahoo.com>
2016-11-28 23:20:37 -0800

    display users on group page

--- Splitzee/Splitzee/NewAdminTransactionViewController.swift
+++ Splitzee/Splitzee/NewAdminTransactionViewController.swift
@@ -30,7 +30,7 @@ class NewAdminTransactionViewController: UIViewController, UICollectionViewDataS
         super.viewDidLoad()
         configureKeyboard()
         setUpUI()
-        pollForUsers()
+        setMemberList()
         setupCollectionView()
         
         // Do any additional setup after loading the view.
@@ -112,6 +112,7 @@ class NewAdminTransactionViewController: UIViewController, UICollectionViewDataS
         collectionView.backgroundColor = UIColor.clear
         
         view.addSubview(collectionView)
+        view.bringSubview(toFront: xButton)
     }
     
     
@@ -133,21 +134,14 @@ class NewAdminTransactionViewController: UIViewController, UICollectionViewDataS
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
     }
     
-    func pollForUsers(){
-        rootRef = FIRDatabase.database().reference()
-        if let rootRef = rootRef {
-            rootRef.child(Constants.DataNames.User).queryOrderedByKey().observe(.childAdded, with: {
-                snapshot in
-                let userKey = snapshot.key as String
-                let userDict = snapshot.value as? [String: AnyObject]
-                let user = User(key: userKey, userDict: userDict!)
-                self.membersList.insert(user, at: 0)
-            })
+    func setMemberList(){
+        group.pollForUsers(withBlock: { user in
+            self.membersList.append(user)
             DispatchQueue.main.async {
                 self.collectionView.reloadData()
                 
             }
-        }
+        })
     }
     
     func pressPay(sender: UIButton!)
@@ -196,6 +190,7 @@ class NewAdminTransactionViewController: UIViewController, UICollectionViewDataS
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // should be returning the number of users
+        print("count:" + String(membersList.count))
         return membersList.count
     }
     
